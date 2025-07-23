ef0eb7df02ed512cdd2bf631a5cb9094c9442dfd
elBarc <axelbarce3@gmail.com>
2025-07-23 01:49:36 -0300

    creo las primeras estructuras de vistas responsivas, quiero modificar en la próxima sesión la vista gral antes de empezar con la lógica

--- src/main/java/com/mycompany/cruz/Cruz.java
+++ src/main/java/com/mycompany/cruz/Cruz.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.cruz;
 
+import views.Dashboard;
+
 /**
  *
  * @author axelb
@@ -11,6 +13,7 @@ package com.mycompany.cruz;
 public class Cruz {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Dashboard principal = new Dashboard();
+        
     }
 }
